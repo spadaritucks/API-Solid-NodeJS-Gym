f1ec9c7365b067a577a4ad914d5844c44534beef
spadaritucks <thiago.spadari02@gmail.com>
2025-09-08 07:59:56 -0300

    feat(errors) : add global error handler feature to errors management

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -30,6 +30,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
             return reply.status(409).send()
         }
         return reply.status(500).send()
+
     }
 
     return reply.status(201).send()
