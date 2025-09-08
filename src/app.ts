051ccf33de1abd6fed4ef4fe3a77ba677eb6e3db
spadaritucks <thiago.spadari02@gmail.com>
2025-09-08 06:15:51 -0300

    feat(users) : add folder http/controllers to responsability separation between routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,10 @@
-
 import fastify from "fastify";
-import z from "zod";
-import { prisma } from "./lib/prisma.js";
-
-
+import { appRoutes } from "./http/routes.js";
 export const app = fastify()
 
+app.register(appRoutes)
 
-app.post("/users", async (request, reply) => {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6),
-    })
 
-    const {name, email, password} = registerBodySchema.parse(request.body)
 
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash : password
-        }
-    })
-
-    return reply.status(201).send()
-})
 
 
