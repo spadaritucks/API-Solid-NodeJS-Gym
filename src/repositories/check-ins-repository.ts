7ed58acaf89da7a47f9dcacf06a1d23cec196018
spadaritucks <thiago.spadari02@gmail.com>
2025-09-09 16:21:45 -0300

    feat(checkins): add checkins metrics feature

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -1,7 +1,8 @@
 import type { CheckIn, Prisma, User } from "generated/prisma/index.js";
 
 export interface CheckInsRepository {
-    create(data : Prisma.CheckInUncheckedCreateInput ) : Promise<CheckIn>
-    findByUserIdOnDate(userId: string, date : Date) : Promise<CheckIn | null>
-    findManyByUserId (userId: string, page : number) : Promise<CheckIn[]>
+    create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+    findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+    findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+    countByUserId(userId: string): Promise<number>
 }
