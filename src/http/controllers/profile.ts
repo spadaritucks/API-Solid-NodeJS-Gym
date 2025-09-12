e22351c04b2c6ea453e2aa736fcf8065fd2d687c
spadaritucks <thiago.spadari02@gmail.com>
2025-09-12 09:06:03 -0300

    feat(profile) : add controller to return user profile and jwtVerification as middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,10 +1,24 @@
-import { InvalidCredentialsError } from "@/services/errors/invalid-credentials-error.js";
+import { makeGetUserProfileService } from "@/services/factories/make-get-user-profile-service.js";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
     
-    
+
+    const getUserProfile = makeGetUserProfileService()
+
+    const {user} = await getUserProfile.execute({
+        userId: request.user.sub
+    })
+
+   
+
+    return reply.status(200).send({
+        user : {
+            ...user,
+            password_hash: undefined
+        }
+    })
 
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,10 +2,11 @@ import type { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.js";
 import { authenticate } from "./controllers/authenticate.js";
 import { profile } from "./controllers/profile.js";
+import { verifyJWT } from "./middlewares/verify-jwt.js";
 
 export async function appRoutes (app : FastifyInstance) {
     app.post("/users", register)
     app.post("/sessions", authenticate)
 
-    app.get("/me", profile)
+    app.get("/me",{onRequest : [verifyJWT]}, profile)
 }
