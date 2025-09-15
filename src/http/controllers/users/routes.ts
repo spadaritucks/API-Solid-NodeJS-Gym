3e06d17502718d2b6d561f10a8eb71b4746623e3
spadaritucks <thiago.spadari02@gmail.com>
2025-09-15 11:28:53 -0300

    feat:(auth) : add refresh token configuration

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,12 +3,14 @@ import { register } from "./register.js";
 import { authenticate } from "./authenticate.js";
 import { verifyJWT } from "@/http/middlewares/verify-jwt.js";
 import { profile } from "./profile.js";
+import { refresh } from "./refresh.js";
 
 
 
 export async function userRoutes (app : FastifyInstance) {
     app.post("/users", register)
     app.post("/sessions", authenticate)
+    app.patch('/token/refresh', refresh)
 
     app.get("/me",{onRequest : [verifyJWT]}, profile)
 }
